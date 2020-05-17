ec5ae4084ed27badd9e6b49042b42cdccff4b1c2
jed1337 <j.caychingco@yahoo.com>
2020-05-17 15:41:56 +0800

    Add button to generate a new random quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import stoicApi from 'stoic-api'
+import styles from './App.module.css'
 
-class App extends React.Component{
+class App extends React.Component {
+    state = {
+        quote:''
+    }
+
+    componentDidMount() {
+        this.getQuote()
+    }
+
+    getQuote = () =>{
+        this.setState({quote: stoicApi.random()})
+    }
+
     render() {
         return (
-            <div>{stoicApi.random()}</div>
+            <div>
+                <div className={styles.quote}>
+                    {this.state.quote}
+                </div>
+                <div>
+                    <button onClick={this.getQuote}>New quote</button>
+                </div>
+            </div>
         )
     }
 }
